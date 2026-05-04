f7a8bb146066653b1099d962b4f02e992a834d3f
Kalemillion <kalemillion@example.com>
2026-05-04 16:25:34 +0200

    translate README and add multiple strings

---
+++ mod.lua
@@ -0,0 +1,21 @@
+function data()
+    return {
+        info = {
+            name           = _("mod_name_i18n"),
+            description    = _("mod_description_i18n"),
+            authors        = {
+                { name = "Kalemillion", role = "CREATOR",         tfnetId = "" },
+                { name = "ConstasJ",    role = "BASED_ON (v1.5)", tfnetId = "" },
+                { name = "H3lyx",       role = "BASED_ON (v1.4)", tfnetId = "" },
+                { name = "Gregory365",  role = "BASED_ON (v1.3)", tfnetId = "" },
+                { name = "Celmi",       role = "BASED_ON",        tfnetId = "" },
+                { name = "quittung",    role = "BASED_ON",        tfnetId = "" },
+            },
+            minorVersion   = 6,
+            severityAdd    = "NONE",
+            severityRemove = "WARNING",
+            params         = {},
+            tags           = { "Script Mod" },
+        },
+    }
+end
